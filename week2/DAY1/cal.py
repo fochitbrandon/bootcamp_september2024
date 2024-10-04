0b3cfd4cbc4f313b6cdc9f8ace83c86403742884
fochitbrandon <forchitbrandon57@gmail.com>
2024-10-04 02:03:37 -0700

    first commit

--- week2/DAY1/cal.py
+++ week2/DAY1/cal.py
@@ -1,34 +1,50 @@
 print("input an option number to perform the operation")
 print("1.Addition")
 print("2.Subtraction")
-print("3.Multiplicatio")
+print("3.Multiplication")
 print("4.Division")
 print("5.Modulos")
 input("input your number ")
 
 a = 1
 b  = 2
-a = 3
-a = 4
-a = 5
+c = 3
+d = 4
+e = 5  
+
 if a == 1 :
- num1 = (input("enter value1: "))
-num2 = (input("enter value2: "))
+ num1 =int (input("enter value1: "))
+num2 = int(input("enter value2: "))
 sum = num1 + num2
-print("the sun is", sum)
+print("the sun is:", sum)
 
  
 
 if b == 2:
 
  num1 =int (input("enter value1: "))
-num2 = int(input("enter value2: "))
-sum = num1 + num2
-product = num1 * num2
-division = num1 / num2
-modulos = num1 % num2
+ num2 = int(input("enter value2: "))
+ SUBTRCTION  = num1 - num2
+ print("the subtraction is : ", SUBTRCTION )
+ 
 
-print( "the sun is", sum)
-print("the product is ",product)
-print("the division is ",division)
-print("the modulos is ",modulos)
+if c == 3 :
+ num1 = int(input("enter value1: "))
+num2 = int(input("enter value2: "))
+product= num1 * num2
+print("the product is:", product)
+
+
+if d == 4 :
+ num1 = int(input("enter value1: "))
+num2 = int(input("enter value2: "))
+division= num1 / num2
+print("the division is:", division)
+
+
+if e == 5 :
+ num1 =int (input("enter value1: "))
+num2 = int(input("enter value2: "))
+modulos= num1 % num2
+print("the modolus is:",modulos)
+
